d19abed787748ab109c14ff216ad37ed6719d69b
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2020-08-22 20:19:27 +0530

    reactdom1

--- react101/src/index.js
+++ react101/src/index.js
@@ -5,6 +5,6 @@ import './index.css';
 ReactDOM.render(
 	<>
 		<h1 className="hello">Hello World</h1>
-		<p>This is new paragraph</p>
+		<p>This is new paragraph Et incididunt nostrud eiusmod culpa ut sunt nisi exercitation duis deserunt labore consequat eiusmod id officia incididunt ullamco elit aliquip ullamco deserunt dolor deserunt enim aliqua elit elit mollit aliquip.</p>
 	</>
 , document.getElementById('root'));
