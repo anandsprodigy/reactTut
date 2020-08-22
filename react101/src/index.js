e6e8aacf5a5f6719dd6fa128979bff70fd09a70d
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2020-08-22 20:16:13 +0530

    react9

--- react101/src/index.js
+++ react101/src/index.js
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+ReactDOM.render(
+	<>
+		<h1 className="hello">Hello World</h1>
+		<p>This is new paragraph</p>
+	</>
+, document.getElementById('root'));
