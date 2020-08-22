1171173f493cedddd0fe62ee7373341bc1e297b1
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2020-08-22 20:47:00 +0530

    rwct

--- react101/src/index.js
+++ react101/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-var path = "https://picsum.photos/200/300";
+const path = "https://picsum.photos/200/300";
 
 ReactDOM.render(
 	<>
 		<h1 className="hello">Hello World</h1>
 		<p>This is new pusmod id officia incididunt ullamco elit aliquip ullamco deserunt dolor deserunt enim aliqua elit elit mollit aliquip.</p>
-		<img src="{path}"></img>
+		<img src="{path}" alt="alternate Image"></img>
 	</>
 , document.getElementById('root'));
