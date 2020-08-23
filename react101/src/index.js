a0872e535d9a40d454e786ad2035cd6598eede88
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2020-08-23 11:26:20 +0530

    demo

--- react101/src/index.js
+++ react101/src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import Navbar_Center from './Navbar_Center';
 
 const image1 = "https://picsum.photos/200/300";
-const image2 = "https://picsum.photos/200/300";
-const image3 = "https://picsum.photos/200/300";
 
 ReactDOM.render(
 	<>
@@ -13,7 +11,5 @@ ReactDOM.render(
 	<h1 className="hello">Hello World</h1>
 	<p>This is new pusmod id officia incididunt ullamco elit aliquip ullamco deserunt dolor deserunt enim aliqua elit elit mollit aliquip.</p>
 	<img src={image1} alt="alternate Image" />
-	<img src={image2} alt="alternate Image23" />
-	<img src={image3} alt="alternate Image3" />
 	</>
 	, document.getElementById('root'));
